e12f1279a2f91ab02ba3281f65467cdb0bbebb52
Mike Freedman <mike@timescale.com>
2026-02-01 00:05:14 -0500

    fix: remove redundant email index from docker-demo
    The users table already has a UNIQUE constraint on email which creates
    an implicit index, making idx_users_email redundant.

--- examples/docker-demo/init.sql
+++ examples/docker-demo/init.sql
@@ -148,7 +148,6 @@ SELECT
 FROM generate_series(1, 8000) AS i;
 
 -- Single-column indexes
-CREATE INDEX idx_users_email ON users(email);
 CREATE INDEX idx_products_category ON products(category);
 CREATE INDEX idx_orders_user_id ON orders(user_id);
 CREATE INDEX idx_orders_product_id ON orders(product_id);
