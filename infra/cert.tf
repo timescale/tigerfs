5ab1d0a217691a049ed737a380d1fe43cd95c167
Mike Freedman <mike@timescale.com>
2026-03-19 00:56:57 -0400

    chore: remove infra/ (moved to private repo timescale/tigerfs-infra)

--- infra/cert.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_acm_certificate" "install" {
-  domain_name       = var.domain_name
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
--- infra/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "domain_name" {
-  description = "Domain name for the install endpoint"
-  type        = string
-  default     = "install.tigerfs.io"
-}
